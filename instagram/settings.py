370fe0fec7f9848a34b2b6a08627850711737b50
careymwarabu <careyakmwarabu@gmail.com>
2021-07-13 01:11:41 +0300

    deploy to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -94,9 +94,9 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
+           'NAME': 'instagram',
+           'USER': 'carey',
+           'PASSWORD': 'carey',
            'HOST': config('DB_HOST'),
            'PORT': '',
        }
